284c593171da42e4f1bc3d6fb095eb504e4bc8ff
MuhammedThajudeen <thajudeen533@gmail.com>
2023-06-01 21:55:42 +0530

    signin_screen_edit

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -26,13 +26,14 @@ class signinscreen extends StatelessWidget {
                 decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(32),
-                        topRight: Radius.circular(32))),
+                        topLeft: Radius.circular(40),
+                        topRight: Radius.circular(40))),
                 child: Column(
                   children: [
                     Container(
-                      padding: EdgeInsets.only(top: 31),
-                      child: Center(
+                      padding: EdgeInsets.only(top: 36, left: 44),
+                      child: Align(
+                        alignment: Alignment.topLeft,
                         child: Text(
                           'Sign In',
                           style: TextStyle(
@@ -42,21 +43,81 @@ class signinscreen extends StatelessWidget {
                         ),
                       ),
                     ),
-                    Container(
-                      padding: EdgeInsets.only(top: 20, left: 41, right: 42),
-                      child: Center(child: TextField()),
+                    SizedBox(
+                      height: 16,
                     ),
                     Container(
-                      padding: EdgeInsets.only(top: 20, left: 41, right: 42),
-                      child: Center(child: TextField()),
+                      margin: EdgeInsets.only(left: 42, right: 42),
+                      padding: EdgeInsets.only(left: 16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color.fromARGB(255, 241, 241, 241),
+                      ),
+                      alignment: Alignment.centerLeft,
+                      height: 44,
+                      child: Text('Enter your phone number'),
                     ),
-                    Text(
-                      'Forgot Password',
-                      textAlign: TextAlign.right,
+                    SizedBox(
+                      height: 15,
                     ),
-                    button(
-                      buttontext: 'Sign In',
-                    )
+                    Container(
+                      margin: EdgeInsets.only(left: 42, right: 42),
+                      padding: EdgeInsets.only(left: 16, right: 16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color.fromARGB(255, 241, 241, 241),
+                      ),
+                      alignment: Alignment.centerLeft,
+                      height: 44,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text('Enter your password'),
+                          Icon(
+                            Icons.remove_red_eye,
+                            size: 16,
+                            color: Color.fromARGB(
+                              255,
+                              205,
+                              205,
+                              205,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      alignment: Alignment.bottomRight,
+                      padding: EdgeInsets.only(right: 27),
+                      child: Text(
+                        'Forgot Password',
+                        style: TextStyle(fontSize: 10),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 27, right: 27),
+                      child: button(
+                        buttontext: 'Sign In',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(bottom: 0),
+                      child: Text(
+                        "Don 't have an account yet? Sign Up",
+                        style: TextStyle(
+                            fontSize: 12,
+                            color: Color.fromARGB(255, 41, 79, 177)),
+                      ),
+                    ),
                   ],
                 ),
               ))
--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -12,18 +12,17 @@ class button extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.only(top: 20, left: 27, right: 27),
-        child: TextButton(
-          style: ButtonStyle(
-              shape: MaterialStatePropertyAll(RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30))),
-              fixedSize: MaterialStatePropertyAll(Size.fromHeight(49)),
-              backgroundColor:
-                  MaterialStatePropertyAll(Color.fromARGB(255, 41, 79, 177))),
-          onPressed: () {},
-          child: Text(buttontext,
-              style: GoogleFonts.overpass(fontSize: 13, color: Colors.white)),
-        ));
+    return TextButton(
+      style: ButtonStyle(
+          shape: MaterialStatePropertyAll(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
+          fixedSize: MaterialStatePropertyAll(
+              Size(MediaQuery.of(context).size.width, 49)),
+          backgroundColor:
+              MaterialStatePropertyAll(Color.fromARGB(255, 41, 79, 177))),
+      onPressed: () {},
+      child: Text(buttontext,
+          style: GoogleFonts.overpass(fontSize: 13, color: Colors.white)),
+    );
   }
 }
