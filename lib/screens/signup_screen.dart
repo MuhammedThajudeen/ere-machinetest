b41563eb2b986d339d8afd0a05d8ee9a7cbb1d8b
MuhammedThajudeen <thajudeen533@gmail.com>
2023-06-03 20:40:33 +0530

    added_signup_screen

---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,158 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unnecessary_import
+
+import 'package:ere_machinetest/screens/verify_screen.dart';
+import 'package:ere_machinetest/widgets/arrow_button.dart';
+import 'package:ere_machinetest/widgets/background_container.dart';
+import 'package:ere_machinetest/widgets/top_backbutton.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class signup extends StatelessWidget {
+  const signup({super.key, req});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          bgcontainer(
+            bigtext: 'Create better\ntogether',
+            smalltext: 'Join our community ',
+          ),
+          screenbackbutton(),
+          Positioned(
+              bottom: 0,
+              child: Container(
+                height: 356,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40),
+                        topRight: Radius.circular(40))),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(top: 36, left: 44),
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                              fontSize: 23,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 42, right: 42),
+                      padding: EdgeInsets.only(left: 16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color.fromARGB(255, 241, 241, 241),
+                      ),
+                      alignment: Alignment.centerLeft,
+                      height: 44,
+                      child: Text('Enter your full name',
+                          style: GoogleFonts.robotoFlex(
+                              fontSize: 11,
+                              fontWeight: FontWeight.w400,
+                              color: Color.fromARGB(255, 198, 198, 198))),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 42, right: 42),
+                      padding: EdgeInsets.only(left: 16, right: 16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color.fromARGB(255, 241, 241, 241),
+                      ),
+                      alignment: Alignment.centerLeft,
+                      height: 44,
+                      child: Text('Enter your email id',
+                          style: GoogleFonts.robotoFlex(
+                              fontSize: 11,
+                              fontWeight: FontWeight.w400,
+                              color: Color.fromARGB(255, 198, 198, 198))),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    //phone number
+                    Container(
+                        margin: EdgeInsets.only(left: 42, right: 42),
+                        padding: EdgeInsets.only(left: 16, right: 16),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color.fromARGB(255, 241, 241, 241),
+                        ),
+                        alignment: Alignment.centerLeft,
+                        height: 44,
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(left: 8),
+                              child: Text(
+                                '+91',
+                                style: TextStyle(
+                                    fontSize: 11, fontWeight: FontWeight.w400),
+                              ),
+                            ),
+                            Icon(
+                              Icons.arrow_drop_down,
+                              color: Colors.black,
+                            ),
+                            Text('Enter your phone number',
+                                style: GoogleFonts.robotoFlex(
+                                    fontSize: 11,
+                                    fontWeight: FontWeight.w400,
+                                    color: Color.fromARGB(255, 198, 198, 198)))
+                          ],
+                        )),
+
+                    //button
+                    arrowbutton(
+                      buttonname: 'Continue',
+                      routescreen: verifyscreen(),
+                      fontstyle: GoogleFonts.robotoFlex(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white),
+                      topvalue: 24,
+                      bottomvalue: 16,
+                    ),
+
+                    //bottom signup text
+                    Container(
+                      padding: EdgeInsets.only(bottom: 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            " Already a member? ",
+                            style: TextStyle(fontSize: 12, color: Colors.black),
+                          ),
+                          Text(
+                            "Sign Up",
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Color.fromARGB(255, 41, 79, 177)),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -1,14 +1,17 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:ere_machinetest/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class button extends StatelessWidget {
-  const button({super.key, required this.buttontext});
+  const button(
+      {super.key, required this.buttontext, required this.routescreen});
 
   final String buttontext;
+  final Widget routescreen;
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +23,10 @@ class button extends StatelessWidget {
               Size(MediaQuery.of(context).size.width, 49)),
           backgroundColor:
               MaterialStatePropertyAll(Color.fromARGB(255, 41, 79, 177))),
-      onPressed: () {},
+      onPressed: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => routescreen));
+      },
       child: Text(buttontext,
           style: GoogleFonts.overpass(fontSize: 13, color: Colors.white)),
     );
