87c705b39dfd9352c7ce96160d6bbe53fc378fe9
MuhammedThajudeen <thajudeen533@gmail.com>
2023-06-08 18:27:37 +0530

    scrollable view

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,133 +13,134 @@ class home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: ListView(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: 63,
+                width: 63,
+                margin: EdgeInsets.only(top: 6, left: 23),
+                child: Image(
+                  fit: BoxFit.fill,
+                  image: AssetImage('assets/logo.jpg'),
+                ),
+              ),
+              InkWell(
+                child: Container(
+                    margin: EdgeInsets.only(top: 19, right: 25),
+                    height: 37,
+                    width: 40,
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Color.fromARGB(255, 4, 85, 191),
+                        ),
+                        borderRadius: BorderRadius.circular(11)),
+                    child: Icon(
+                      Icons.search,
+                      color: Color.fromARGB(255, 4, 85, 191),
+                    )),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 25, right: 25),
+            width: MediaQuery.of(context).size.width,
+            height: 207,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  Color.fromARGB(255, 40, 77, 173),
+                  Color.fromARGB(255, 15, 52, 147)
+                ]),
+                borderRadius: BorderRadius.circular(25),
+                color: Colors.blue),
+            child: Column(
               children: [
                 Container(
-                  margin: EdgeInsets.only(top: 54, left: 23),
-                  child: Image(
-                    image: AssetImage('assets/logo.jpg'),
-                    height: 63,
-                    width: 63,
+                  margin: EdgeInsets.only(top: 34),
+                  child: Text(
+                    'Area of Funding',
+                    style: GoogleFonts.robotoFlex(
+                        fontSize: 26, color: Colors.white),
                   ),
                 ),
                 Container(
-                  margin: EdgeInsets.only(top: 54, right: 25),
-                  height: 37,
-                  width: 40,
+                  margin: EdgeInsets.only(left: 52, top: 7, right: 52),
+                  child: Text(
+                    'Lorem Ipsum is simply dummy text of the printing',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.robotoFlex(
+                      fontSize: 15,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 107,
+                  height: 38,
+                  margin: EdgeInsets.only(top: 22, bottom: 24),
                   decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Color.fromARGB(255, 4, 85, 191),
-                      ),
+                      color: Colors.white,
                       borderRadius: BorderRadius.circular(11)),
-                  child: IconButton(
+                  child: TextButton(
                       onPressed: () {},
-                      icon: Icon(
-                        Icons.search,
-                        color: Color.fromARGB(255, 4, 85, 191),
+                      child: Text(
+                        'Apply',
+                        style: GoogleFonts.robotoFlex(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w400,
+                            color: Color.fromARGB(255, 37, 74, 170)),
                       )),
-                )
+                ),
               ],
             ),
-            SizedBox(
-              height: 25,
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 25, right: 25),
-              width: MediaQuery.of(context).size.width,
-              height: 207,
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                    Color.fromARGB(255, 40, 77, 173),
-                    Color.fromARGB(255, 15, 52, 147)
-                  ]),
-                  borderRadius: BorderRadius.circular(25),
-                  color: Colors.blue),
-              child: Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 34),
-                    child: Text(
-                      'Area of Funding',
-                      style: GoogleFonts.robotoFlex(
-                          fontSize: 26, color: Colors.white),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(left: 52, top: 7, right: 52),
-                    child: Text(
-                      'Lorem Ipsum is simply dummy text of the printing',
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.robotoFlex(
-                        fontSize: 15,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    width: 107,
-                    height: 38,
-                    margin: EdgeInsets.only(top: 22, bottom: 24),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(11)),
-                    child: TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Apply',
-                          style: GoogleFonts.robotoFlex(
-                              fontSize: 15,
-                              fontWeight: FontWeight.w400,
-                              color: Color.fromARGB(255, 37, 74, 170)),
-                        )),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 13,
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 25),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Lorem Ipsum',
-                  style: GoogleFonts.robotoFlex(
-                    fontSize: 20,
-                    color: Colors.black,
-                  ),
+          ),
+          SizedBox(
+            height: 13,
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 25),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Lorem Ipsum',
+                style: GoogleFonts.robotoFlex(
+                  fontSize: 20,
+                  color: Colors.black,
                 ),
               ),
             ),
-            SizedBox(
-              height: 13,
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  homecontainer(
-                    title: 'Agriculture',
-                    subtitle:
-                        'Lorem Ipsum is simply dummy \ntext of the printing',
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  homecontainer(
-                      title: 'Logistics',
-                      subtitle:
-                          'Lorem Ipsum is simply dummy \ntext of the printing'),
-                ],
+          ),
+          SizedBox(
+            height: 13,
+          ),
+          Column(
+            children: [
+              homecontainer(
+                title: 'Agriculture',
+                subtitle: 'Lorem Ipsum is simply dummy \ntext of the printing',
               ),
-            )
-          ],
-        ),
+              SizedBox(
+                height: 15,
+              ),
+              homecontainer(
+                  title: 'Logistics',
+                  subtitle:
+                      'Lorem Ipsum is simply dummy \ntext of the printing'),
+              SizedBox(
+                height: 15,
+              ),
+              homecontainer(
+                  title: 'Logistics',
+                  subtitle:
+                      'Lorem Ipsum is simply dummy \ntext of the printing'),
+            ],
+          )
+        ],
       ),
       bottomNavigationBar: ClipRRect(
         borderRadius: BorderRadius.only(
